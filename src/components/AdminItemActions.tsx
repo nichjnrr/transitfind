9ba5632f6728b1159de64ee70ba181b9ff601325
nichjnrr <161096234+nichjnrr@users.noreply.github.com>
2026-07-14 09:53:39 +0800

    Create AdminItemActions.tsx

---
+++ src/components/AdminItemActions.tsx
@@ -0,0 +1,14 @@
+// src/components/AdminItemActions.tsx
+"use client";
+// js using it as placeholder first
+export default function AdminItemActions({
+  itemId,
+  itemType,
+  status,
+}: {
+  itemId: string;
+  itemType: "lost" | "found";
+  status: string;
+}) {
+  return null;
+}
