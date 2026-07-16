10d77cbd64a85e45edf1384746396302338389db
Pio Lee <piolee711@gmail.com>
2026-07-16 12:45:57 +0800

    feat: wrap app with ThemeProvider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,15 @@
-// src/app/providers.tsx
 "use client";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import { ThemeProvider } from "@/lib/ThemeContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <Toaster position="top-right" />
-      {children}
+      <ThemeProvider>
+        <Toaster position="top-right" />
+        {children}
+      </ThemeProvider>
     </SessionProvider>
   );
 }
